9d8f0aa0640576c03288886ab99d082bdc8b8171
Vivaldo Rupang <vivaldord@gmail.com>
2023-05-26 20:55:25 +0800

    first commit

--- javascript/crop.js
+++ javascript/crop.js
@@ -18,14 +18,14 @@ const [image, back, crop] = [
   
   back.addEventListener('click', () => {
     localStorage.removeItem('url')
-    window.location.href = 'profile.html'
+    window.location.href = 'changephoto.html'
   })
   
   crop.addEventListener('click', () => {
     const croppedImage = cropper.getCroppedCanvas().toDataURL('image/png')
     localStorage.setItem('url', croppedImage)
     setTimeout(() => {
-      window.location.href = 'profile.html'
+      window.location.href = 'changephoto.html'
     }, 500)
   })
   
